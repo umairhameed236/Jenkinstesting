fd63cf2b0a292503584878ff7dba93c0e8b184f7
umairhameed236 <101642844+umairhameed236@users.noreply.github.com>
2022-10-27 15:08:32 +0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,9 +7,6 @@ echo 'Application in Testing Phase…'
 }
 }
 stage('Deploy CloudHub') {
-environment {
-ANYPOINT_CREDENTIALS = credentials('anypointPlatform')
-}
 steps {
 echo 'Deploying mule project due to the latest code commit…'
 echo 'Deploying to the configured environment….'
