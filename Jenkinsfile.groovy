d003fd453bcb751d4f13cab1d52492bc7dbf682a
umairhameed236 <101642844+umairhameed236@users.noreply.github.com>
2022-10-27 15:02:43 +0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,25 +1,20 @@
 pipeline {
-  
-  agent {
-    label 'master'
-  }
-
-  options {
-    ansiColor('xterm')
-  }
-  
-  
-  
-  stages {
-    
-    stage('Git Checkout') 
-    {
-      steps{
-        script {
-          echo "Welcome"
-
-
-         
+agent any
+stages {
+stage('Test') {
+steps {
+echo 'Application in Testing Phase…'
 }
-      }
-    }
+}
+stage('Deploy CloudHub') {
+environment {
+ANYPOINT_CREDENTIALS = credentials('anypointPlatform')
+}
+steps {
+echo 'Deploying mule project due to the latest code commit…'
+echo 'Deploying to the configured environment….'
+
+}
+}
+}
+}
