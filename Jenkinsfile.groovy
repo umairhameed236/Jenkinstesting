14f2b10306fada78c853979c113ad64c9eb08f2d
umairhameed236 <101642844+umairhameed236@users.noreply.github.com>
2022-11-15 18:20:43 +0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,30 +1,16 @@
 pipeline {
-agent any
-stages {
-stage('Test') {
-steps {
-echo 'Application in Testing Phase…'
-  echo 'Application Tested'
-}
-}
-stage('Deploy CloudHub') {
-steps {
-echo 'Deploying mule project due to the latest code commit…'
-echo 'Deploying to the configured environment….'
-
-}
-}
-}
-    catch (Exception e) {
-        print e.message
-         if (!(e instanceof InterruptedException || (e.message != null && e.message.contains("task was cancelled"))) && (BRANCH_NAME != 'dev')) {
-            currentBuild.result = 'FAILURE'
-
-            step([$class                  : 'Mailer',
-                  notifyEveryUnstableBuild: true,
-                  recipients              : 'umair.hameed236@gmail.com',
-                  sendToIndividuals       : true])
+    agent any
+    
+    stages {
+        stage('Ok') {
+            steps {
+                echo "Ok"
+            }
+        }
+    }
+    post {
+        always {
+            emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
         }
     }
 }
-
